c3b6a02334f40be16533b18967a5afaf7bb0c808
Sriram N <43564446+Sriram-n-27@users.noreply.github.com>
2020-07-09 19:02:47 +0530

    remove folder

--- FoodMart (Regular User)/app/src/main/java/com/sriram_n/foodmart/Common/Common.java
+++
@@ -1,7 +0,0 @@
-package com.sriram_n.foodmart.Common;
-
-import com.sriram_n.foodmart.Model.User;
-
-public class Common {
-    public static User currentUser;
-}
--- FoodMart (Regular User)/app/src/main/java/com/sriram_n/foodmart/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.sriram_n.foodmart.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onCLick(View view, int position, boolean isLongClick);
-}
